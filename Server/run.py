bdc3446232da7f7baf50df6d61b4ea372c4fe5d6
Niewidzialny84 <niewidzialny84@vp.pl>
2021-04-18 15:33:42 +0200

    Server console working tweaks

---
+++ Server/run.py
@@ -0,0 +1,3 @@
+from server import ConsoleApp
+
+app = ConsoleApp(9999)
--- Server/server.py
+++ Server/server.py
@@ -1,39 +1,55 @@
 import socket
 import threading
 
+import time
+import datetime
+import pytz
+
 class Server:
-    def __init__(self):
+    def __init__(self,port):
         #This should get ip address of the card
-        self.ip = socket.gethostbyname(socket.gethostname())
+        #self.ip = socket.gethostbyname(socket.gethostname())
         #This should make run socket on all interfaces
         self.ip = ""
-        self.port = 9999
+        self.port = port
         self.running = True
+
+        self.timezone = pytz.timezone('Europe/Warsaw')
 
         try:
             #Binding adress and settings to socket
             self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
             self.sock.bind((self.ip,self.port))
 
-            print('Running on: %s and port %s' % (self.ip, self.port))
         except:
-            print('Something went wrong on port bind')
+            self.log('Something went wrong on port bind')
         
         self.connections = []
         
 
     def run(self):
+        self.log('Running on: ' + str(self.ip) + ':' + str(self.port) )
+
         self.sock.listen(50)
 
         #Awating loop for connections
         while self.running:
-            connection, address = self.sock.accept()
-            self.connections.append(connection)
-            #Creating new thread for every client
-            threading.Thread(target=self.handle,args=(connection,address)).start()
-        
+            self.log('Awaiting more connections...')
+            try: 
+                connection, address = self.sock.accept()
+                self.log(str(address) + ' connected')
+                self.connections.append(connection)
+                #Creating new thread for every client
+                threading.Thread(target=self.handle,args=(connection,address)).start()
+            except WindowsError:
+                pass
+            except Exception as err:
+                self.log('Some error occured ' + str(err))
+                pass
+
     def handle(self,connection,address):
-        while self.running:
+        loop = False
+        while self.running and not loop:
             try:
                 #Data handling
                 #FIXME: Add more info into packets and split incoming data
@@ -42,6 +58,8 @@ class Server:
             except socket.error:
                 #Close connection on fail and remove from connections list
                 connection.close()
+                self.log(str(connection))
+                loop = True
                 # self.connections.remove(connection)
 
     def send(self,connection,data):
@@ -53,5 +71,34 @@ class Server:
                 except:
                     pass
 
-server = Server()
-server.run()
+    def log(self, message: str):
+        #Console logging on time
+        t = '[ '+ datetime.datetime.now(self.timezone).strftime("%Y-%m-%d %H:%M:%S")+' ]  '
+        print(t + str(message))
+
+    def stop(self):
+        self.running = False
+        self.sock.close()
+
+class ConsoleApp:
+    def __init__(self, port: int):
+        #self.ipaddr = ipaddr
+        self.port = port
+
+        self.server = Server(self.port)
+
+        self.thread = threading.Thread(target=self.server.run)      
+
+        self.server.log('Starting server')
+        self.thread.start()
+        
+        self.run()
+
+    def run(self):
+        while(True):
+            val = str(input())
+            if val.upper() == 'STOP':
+                self.server.log('Stopping server...')
+                self.server.stop()
+                break
+                
